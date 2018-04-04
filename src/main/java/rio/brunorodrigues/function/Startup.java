dc39f840f3257b2c77557c616efcf63538a20d2a
Bruno Rodrigues <brrodrigues@live.com>
2018-04-04 18:07:08 -0300

    Configuring user service's route

--- src/main/java/rio/brunorodrigues/function/Startup.java
+++ src/main/java/rio/brunorodrigues/function/Startup.java
@@ -1,16 +1,25 @@
 package rio.brunorodrigues.function;
 
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.HandlerAdapter;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.HandlerMapping;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "rio.brunorodrigues.function")
@@ -31,6 +40,38 @@ public class Startup extends SpringBootServletInitializer {
     public HandlerMapping handlerMapping() {
         return new RequestMappingHandlerMapping();
     }
+
+    @Bean
+    public ObjectMapper objectMapper(){
+        ObjectMapper mapper = new ObjectMapper();
+
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,false);
+        mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+        mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        mapper.configure(JsonParser.Feature.ALLOW_COMMENTS, true);
+        mapper.configure(JsonParser.Feature.ALLOW_UNQUOTED_FIELD_NAMES, true);
+        mapper.configure(JsonParser.Feature.ALLOW_SINGLE_QUOTES, true);
+        return mapper;
+
+    }
+
+    @Bean
+    public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
+        MappingJackson2HttpMessageConverter jsonConverter = new MappingJackson2HttpMessageConverter();
+
+        List<MediaType> lstMedia = new ArrayList();
+        lstMedia.add(MediaType.APPLICATION_JSON_UTF8);
+        lstMedia.add(MediaType.APPLICATION_JSON);
+        lstMedia.add(MediaType.TEXT_HTML);
+        lstMedia.add(MediaType.TEXT_PLAIN);
+        lstMedia.add(MediaType.ALL);
+
+        jsonConverter.setObjectMapper(objectMapper());
+        jsonConverter.setSupportedMediaTypes(lstMedia);
+        return jsonConverter;
+
+    }
+
 
     /*
      * Create required HandlerAdapter, to avoid several default HandlerAdapter instances being created
--- src/main/java/rio/brunorodrigues/function/controller/UserController.java
+++ src/main/java/rio/brunorodrigues/function/controller/UserController.java
@@ -1,6 +1,5 @@
 package rio.brunorodrigues.function.controller;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import rio.brunorodrigues.function.data.UserData;
